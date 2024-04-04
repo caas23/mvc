3e95074b668c33185bfed3edb880be13000b014c
caas23 <caas23@Calle-Dator>
2024-04-04 20:45:15 +0200

    moved api route to separate controller

--- src/Controller/Kmom01ControllerTwig.php
+++ src/Controller/Kmom01ControllerTwig.php
@@ -43,23 +43,4 @@ class Kmom01ControllerTwig extends AbstractController
 
         return $this->render('Kmom01/lucky.html.twig', $data);
     }
-
-
-    #[Route("/api", name: "api")]
-    public function api(): Response
-    {
-        $number = random_int(100, 999);
-        $num1 = str_split($number, 1)[0];
-        $num2 = str_split($number, 1)[1];
-        $num3 = str_split($number, 1)[2];
-
-        $data = [
-            'number' => $number,
-            'num1' => $num1,
-            'num2' => $num2,
-            'num3' => $num3
-        ];
-
-        return $this->render('Shared/api.html.twig', $data);
-    }
 }
