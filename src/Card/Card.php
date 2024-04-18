9a9f55187568c4e3efeecbe65dd8c9cd0536790a
caas23 <caas23@Calle-Dator>
2024-04-18 17:48:48 +0200

    added getValue

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -4,7 +4,7 @@ namespace Caas23\Card;
 
 class Card
 {
-    protected mixed $card;
+    protected ?string $card;
 
     public function __construct()
     {
@@ -12,13 +12,21 @@ class Card
     }
 
     /**
-     * @param array<mixed> $cards
+     * @param array $cards
      */
-    public function getOneCard(array $cards): mixed
+    public function getOneCard(array $cards): string
     {
         $randKey = array_rand($cards, 1);
 
         $this->card = $cards[$randKey];
         return $this->card;
+    }
+
+    public function getValue(string $card): int
+    {
+        $number = explode("-", $card)[1];
+        $number = explode(".", $number)[0];
+
+        return (int)$number;
     }
 }
