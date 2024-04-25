da462fee018ca2240e0f6a025d29070f7e945b4e
caas23 <caas23@Calle-Dator>
2024-04-25 19:23:34 +0200

    added comments, adjusted path to cards

--- src/Card/DeckOfCards.php
+++ src/Card/DeckOfCards.php
@@ -2,6 +2,9 @@
 
 namespace Caas23\Card;
 
+/**
+ * DeckOfCards class handling a deck.
+ */
 class DeckOfCards
 {
     protected mixed $deck;
@@ -12,20 +15,22 @@ class DeckOfCards
     }
 
     /**
+     * Get deck of cards.
      * @return array<mixed>
      */
-    public function getCards(): array
+    public function getCards(string $path): array
     {
-        $this->deck = array_diff((array)scandir('../public/svg/'), array('..', '.', '_joker1.svg', '_joker2.svg'));
+        $this->deck = array_diff((array)scandir($path), array('..', '.', '_joker1.svg', '_joker2.svg'));
         return $this->deck;
     }
 
     /**
+     * Get deck of cards shuffled.
      * @return array<mixed>
      */
-    public function getCardsShuffled(): array
+    public function getCardsShuffled(string $path): array
     {
-        $this->deck = $this->getCards();
+        $this->deck = $this->getCards($path);
         shuffle($this->deck);
 
         return $this->deck;
